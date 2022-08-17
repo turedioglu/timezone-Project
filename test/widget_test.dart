fe4f82cd3c91743170df4e004dc52ccffff26837
turedioglu <fahrettin098@hotmail.com>
2022-08-18 01:09:11 +0300

    added unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,14 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:timezone_by_area/main.dart';
+import 'package:timezone_by_area/core/services/api/home_service.dart';
+import 'package:timezone_by_area/utils/settings/locator.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  setUp(() async {
+    setupLocator();
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Get Location Detail', () async {
+    final response = await HomeService().getLocationDetail('Asia/Omsk');
+    expect(response.timezone, isNotEmpty);
   });
 }
